a7e22758966adbc958d3167f953ddeb3b63fb93d
Camden Coe <cxcoe@ilstu.edu>
2016-11-10 04:32:00 -0600

    end my miserable existence

--- app/src/main/java/com/example/brandon/it226project2/LocationAlarm.java
+++ app/src/main/java/com/example/brandon/it226project2/LocationAlarm.java
@@ -1,13 +1,184 @@
 package com.example.brandon.it226project2;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Criteria;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.media.Ringtone;
+import android.media.RingtoneManager;
+import android.net.Uri;
 import android.os.Bundle;
 
-public class LocationAlarm extends AppCompatActivity {
+import android.os.CountDownTimer;
+import android.os.Looper;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.app.NotificationCompat;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
-    @Override
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import static java.lang.Math.abs;
+
+
+public class LocationAlarm extends AppCompatActivity
+{
+    Uri alert;
+    Ringtone r;
+    int numAlarms;
+    double oldLat;
+    double oldLon;
+    double newLat;
+    double newLon;
+    private Button mMake;
     protected void onCreate(Bundle savedInstanceState) {
+        oldLat = 0;
+        oldLon = 0;
+        newLat = 0;
+        newLon = 0;
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_location_alarm);
+        numAlarms = 0;
+
+        final EditText mMin = (EditText) findViewById(R.id.min_move);
+        mMake = (Button) findViewById(R.id.start_time);
+
+        r = RingtoneManager.getRingtone(getApplicationContext(), alert);
+        alert = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_ALARM);
+
+        Criteria criteria = new Criteria();
+        criteria.setAccuracy(Criteria.ACCURACY_COARSE);
+        criteria.setPowerRequirement(Criteria.POWER_LOW);
+        criteria.setAltitudeRequired(false);
+        criteria.setBearingRequired(false);
+        criteria.setSpeedRequired(false);
+        criteria.setCostAllowed(true);
+        criteria.setHorizontalAccuracy(Criteria.ACCURACY_HIGH);
+        criteria.setVerticalAccuracy(Criteria.ACCURACY_HIGH);
+        Looper looper = null;
+
+        if (ContextCompat.checkSelfPermission(this,
+                android.Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED) {
+
+
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this,
+                    android.Manifest.permission.ACCESS_FINE_LOCATION)) {
+
+            }
+            else
+            {
+                ActivityCompat.requestPermissions(this,
+                        new String[]{android.Manifest.permission.ACCESS_FINE_LOCATION},
+                        5);
+
+            }
+        }
+        LocationManager locationManager = (LocationManager)getSystemService(Context.LOCATION_SERVICE);
+
+        LocationListener locationListener = new LocationListener() {
+            @Override
+            public void onLocationChanged(Location location) {
+
+                newLat = location.getLatitude();
+                newLon = location.getLongitude();
+            }
+
+            public void onStatusChanged(String provider, int status, Bundle extras) {
+                //Log.d("Status Changed", String.valueOf(status));
+            }
+
+            public void onProviderEnabled(String provider) {
+                //Log.d("Provider Enabled", provider);
+            }
+
+            public void onProviderDisabled(String provider) {
+                //Log.d("Provider Disabled", provider);
+            }
+        };
+        locationManager.requestSingleUpdate(criteria, locationListener, looper);
+        //locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 0, 0, locationListener);
+
+        mMake.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String help = mMin.getText().toString();
+                makeAlarm(v, help, oldLat, oldLon);
+            }
+        });
+    }
+
+    public void makeAlarm(final View v, String min, double lat, double lon) {
+        final TextView mText = (TextView) findViewById(R.id.newText);
+        long minInput = Long.parseLong(min) * 6000;
+        final int notificationID = numAlarms;
+        final String minHelp = min;
+        numAlarms++;
+        final NotificationManager mNotificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+        final NotificationCompat.Builder mBuilder = new NotificationCompat.Builder(getApplicationContext());
+        mBuilder.setSmallIcon(R.drawable.notification_icon);
+        Intent intent = new Intent(getApplicationContext(), LocationAlarm.class);
+        intent.setAction(Intent.ACTION_MAIN);
+        intent.addCategory(Intent.CATEGORY_LAUNCHER);
+        PendingIntent pendingIntent = PendingIntent.getActivity(getApplicationContext(), 0, intent, 0);
+        mBuilder.setContentIntent(pendingIntent);
+        oldLat = newLat;
+        oldLon = newLon;
+        new CountDownTimer(minInput, 1000) {
+            public void onTick(long millisUntilFinished) {
+                String format = new SimpleDateFormat("mm:ss").format(new Date(millisUntilFinished));
+                mText.setText("Time Remaining: " + format);
+                mBuilder.setContentTitle("Keep moving!");
+                mBuilder.setContentText("Time left: " + format);
+                mNotificationManager.notify(notificationID, mBuilder.build());
+            }
+
+            public void onFinish() {
+                if(abs(oldLon - newLon) < 1 || abs(oldLat - newLat) < 1)
+                {
+                    mBuilder.setContentTitle("Get up and walk!");
+                    mBuilder.setContentText("Get moving");
+                    mNotificationManager.notify(notificationID, mBuilder.build());
+                    mText.setText("Move!");
+                    startNoise(v);
+                }
+                else
+                {
+                    oldLon = newLon;
+                    oldLat = newLat;
+                    makeAlarm(v, minHelp, newLat, newLon);
+                    cancel();
+                }
+
+            }
+        }.start();
+
+
+    }
+
+    public void updateLocation()
+    {
+
+    }
+
+    public void startNoise(View v) {
+        try {
+            Uri notification = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
+            Ringtone r = RingtoneManager.getRingtone(getApplicationContext(), notification);
+            r.play();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
+
--- app/src/main/java/com/example/brandon/it226project2/TimerAlarm.java
+++ app/src/main/java/com/example/brandon/it226project2/TimerAlarm.java
@@ -1,16 +1,25 @@
 package com.example.brandon.it226project2;
 
+import android.Manifest;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.app.TaskStackBuilder;
 import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Criteria;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.media.AudioManager;
 import android.media.MediaPlayer;
 import android.media.Ringtone;
 import android.media.RingtoneManager;
 import android.net.Uri;
 import android.os.CountDownTimer;
+import android.os.Looper;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.app.NotificationCompat;
@@ -23,13 +32,17 @@ import java.io.IOException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
-public class TimerAlarm extends AppCompatActivity
-{
+public class TimerAlarm extends AppCompatActivity {
     Uri alert;
     Ringtone r;
     int numAlarms;
+    double lat;
+    double lon;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        lat = 0;
+        lon = 0;
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_timer_alarm);
         numAlarms = 0;
@@ -37,9 +50,68 @@ public class TimerAlarm extends AppCompatActivity
         final EditText mMin = (EditText) findViewById(R.id.setMin);
         final Button mMake = (Button) findViewById(R.id.startTimer);
         final EditText mMessage = (EditText) findViewById(R.id.customMessage);
-
         r = RingtoneManager.getRingtone(getApplicationContext(), alert);
         alert = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_ALARM);
+        Criteria criteria = new Criteria();
+        criteria.setAccuracy(Criteria.ACCURACY_COARSE);
+        criteria.setPowerRequirement(Criteria.POWER_LOW);
+        criteria.setAltitudeRequired(false);
+        criteria.setBearingRequired(false);
+        criteria.setSpeedRequired(false);
+        criteria.setCostAllowed(true);
+        criteria.setHorizontalAccuracy(Criteria.ACCURACY_HIGH);
+        criteria.setVerticalAccuracy(Criteria.ACCURACY_HIGH);
+        Looper looper = null;
+
+        if (ContextCompat.checkSelfPermission(this,
+                android.Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED) {
+
+            // Should we show an explanation?
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this,
+                    android.Manifest.permission.ACCESS_FINE_LOCATION)) {
+
+                // Show an expanation to the user *asynchronously* -- don't block
+                // this thread waiting for the user's response! After the user
+                // sees the explanation, try again to request the permission.
+
+            } else {
+
+                // No explanation needed, we can request the permission.
+
+                ActivityCompat.requestPermissions(this,
+                        new String[]{android.Manifest.permission.ACCESS_FINE_LOCATION},
+                        5);
+
+                // MY_PERMISSIONS_REQUEST_READ_CONTACTS is an
+                // app-defined int constant. The callback method gets the
+                // result of the request.
+            }
+        }
+        LocationManager locationManager = (LocationManager)getSystemService(Context.LOCATION_SERVICE);
+
+        LocationListener locationListener = new LocationListener() {
+            @Override
+            public void onLocationChanged(Location location) {
+
+                lat = location.getLatitude();
+                lon = location.getLongitude();
+            }
+
+            public void onStatusChanged(String provider, int status, Bundle extras) {
+                //Log.d("Status Changed", String.valueOf(status));
+            }
+
+            public void onProviderEnabled(String provider) {
+                //Log.d("Provider Enabled", provider);
+            }
+
+            public void onProviderDisabled(String provider) {
+                //Log.d("Provider Disabled", provider);
+            }
+        };
+        locationManager.requestSingleUpdate(criteria, locationListener, looper);
+        //locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 0, 0, locationListener);
         mMake.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
                 makeAlarm(v, mDay, mMin, mMessage);
@@ -47,8 +119,7 @@ public class TimerAlarm extends AppCompatActivity
         });
     }
 
-    public void makeAlarm(final View v, EditText day, EditText min, EditText message)
-    {
+    public void makeAlarm(final View v, EditText day, EditText min, EditText message) {
         final TextView mText = (TextView) findViewById(R.id.mText);
         long dayInput = Long.parseLong(day.getText().toString()) * 86400000;
         long minInput = Long.parseLong(min.getText().toString()) * 60000;
@@ -59,25 +130,21 @@ public class TimerAlarm extends AppCompatActivity
         final NotificationCompat.Builder mBuilder = new NotificationCompat.Builder(getApplicationContext());
         mBuilder.setSmallIcon(R.drawable.notification_icon);
 
-        Intent intent =  new Intent(getApplicationContext(), TimerAlarm.class);
+        Intent intent = new Intent(getApplicationContext(), TimerAlarm.class);
         intent.setAction(Intent.ACTION_MAIN);
         intent.addCategory(Intent.CATEGORY_LAUNCHER);
         PendingIntent pendingIntent = PendingIntent.getActivity(getApplicationContext(), 0, intent, 0);
         mBuilder.setContentIntent(pendingIntent);
-
-        new CountDownTimer(dayInput + minInput, 1000)
-        {
-            public void onTick(long millisUntilFinished)
-            {
+        new CountDownTimer(dayInput + minInput, 1000) {
+            public void onTick(long millisUntilFinished) {
                 String format = new SimpleDateFormat("mm:ss").format(new Date(millisUntilFinished));
                 mText.setText("Time Remaining: " + format);
                 mBuilder.setContentTitle(title);
-                mBuilder.setContentText("Time left: " + format);
+                mBuilder.setContentText("Time left: " + format + "   Location set at: " + lat + " " + lon);
                 mNotificationManager.notify(notificationID, mBuilder.build());
             }
 
-            public void onFinish()
-            {
+            public void onFinish() {
                 mBuilder.setContentTitle(title);
                 mBuilder.setContentText("Your timer has finished!");
                 mNotificationManager.notify(notificationID, mBuilder.build());
@@ -87,8 +154,12 @@ public class TimerAlarm extends AppCompatActivity
         }.start();
     }
 
-    public void startNoise(View v)
+    public void updateLocation()
     {
+
+    }
+
+    public void startNoise(View v) {
         try {
             Uri notification = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
             Ringtone r = RingtoneManager.getRingtone(getApplicationContext(), notification);
@@ -97,4 +168,6 @@ public class TimerAlarm extends AppCompatActivity
             e.printStackTrace();
         }
     }
+
+
 }
